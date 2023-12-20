2a27773a9c95886bfbec6ae723c3d62fc08724a6
mygit <vuttipat.rungrojbribun@gmail>
2023-12-20 15:38:44 +0700

    Assignment:Dear

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import "./App.css";
-
+import { Button } from "./components/Button.jsx";
+import { Alert } from "./components/Alert.jsx";
+import frown from "./assets/frown.svg";
+import alertTriangle from "./assets/alert-triangle.svg";
+import alertCircle from "./assets/alert-circle.svg";
+import checkCircle from "./assets/check-circle.svg";
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */}
+        <Button />
+        <Button color="#07A4E8" />
+        <Button color="#DE5753" />
+        <Button color="#26B795" />
       </div>
       <hr />
       <div className="alert-components-section">
-        {/* Render ตัว Alert 4 แบบ */}
+        <Alert
+          alertColor="#F9C8C8"
+          alertImage={frown}
+          textAlert="This is error alert box"
+        />
+        <Alert
+          alertColor="#F9D9C8"
+          alertImage={alertTriangle}
+          textAlert="This is warning alert box"
+        />
+        <Alert
+          alertColor="#F9EBC8"
+          alertImage={alertCircle}
+          textAlert="This is info alert box"
+        />
+        <Alert
+          alertColor="#CEF7CD"
+          alertImage={checkCircle}
+          textAlert="This is success alert box"
+        />
       </div>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,25 @@
-// Start coding here
-
-function Button() {}
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+export function Button({ color = "#074EE8" }) {
+  return (
+    <button
+      css={css`
+        display: flex;
+        height: 50px;
+        width: 170px;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+        flex-shrink: 0;
+        border-radius: 4px;
+        font-size: 16px;
+        font-style: normal;
+        color: #fff;
+        margin: 10px;
+        background: ${color};
+      `}
+    >
+      Button
+    </button>
+  );
+}
